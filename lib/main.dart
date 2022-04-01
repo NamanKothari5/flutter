fdbbee52bb809de76cb8193bc8cb1e97ca31ec41
NamanKothari5 <namankothari0512@gmail.com>
2022-04-01 11:52:26 +0530

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catlog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+    var day = 5;
+    const pi = 3.14;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
