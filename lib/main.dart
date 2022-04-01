5d5600408fd056de30e9cc1d5dadab1a0c984caa
NamanKothari5 <namankothari0512@gmail.com>
2022-04-01 12:37:10 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catlog/home_page.dart';
+import 'package:flutter_catlog/pages/home_page.dart';
+import 'package:flutter_catlog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,15 @@ class MyApp extends StatelessWidget {
     const pi = 3.14;
 
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
